b50c54f6b1ee03fca8d0f459dc0d794f027b7a1d
landon.j.fuller <landon.j.fuller@675f16f6-5868-fe76-6977-53118d06ce56>
2012-06-30 20:12:01 +0000

    Checkpoint commit

--- CP210x/CP210x.cpp
+++ CP210x/CP210x.cpp
@@ -1,1 +1,61 @@
-/* add your code here */
+/*
+ * Author: Landon Fuller <landonf@plausible.coop>
+ *
+ * Copyright (c) 2012 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <IOKit/IOLib.h>
+#include "CP210x.h"
+
+// Define the superclass
+#define super IOService
+
+OSDefineMetaClassAndStructors(coop_plausible_driver_CP210x, IOService);
+
+// from IOService base class
+IOService *coop_plausible_driver_CP210x::probe (IOService *provider, SInt32 *score) {
+    IOService *res = super::probe(provider, score);
+    IOLog("IOKitTest::probe\n");
+    return res;
+}
+
+// from IOService base class
+bool coop_plausible_driver_CP210x::start (IOService *provider) {
+    bool res = super::start(provider);
+    IOLog("IOKitTest::start\n");
+    return res;
+}
+
+// from IOService base class
+void coop_plausible_driver_CP210x::stop (IOService *provider) {
+    IOLog("IOKitTest::stop\n");
+    super::stop(provider);
+}
+
+// from IOService base class
+void coop_plausible_driver_CP210x::free (void) {
+    IOLog("IOKitTest::free\n");
+    super::free();
+}
--- CP210x/CP210x.h
+++ CP210x/CP210x.h
@@ -1,1 +1,54 @@
-/* add your code here */
+/*
+ * Author: Landon Fuller <landonf@plausible.coop>
+ *
+ * Copyright (c) 2012 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <IOKit/IOService.h>
+
+#include <IOKit/serial/IOSerialDriverSync.h>
+#include <IOKit/serial/IORS232SerialStreamSync.h>
+
+#include <IOKit/usb/USB.h>
+
+class coop_plausible_driver_CP210x : public IOService {
+    OSDeclareDefaultStructors(coop_plausible_driver_CP210x);
+    
+private:
+    /** Our published rs232 nub */
+    IORS232SerialStreamSync *nub;
+
+public:
+    
+    // IOKit methods:
+    
+	virtual IOService *probe (IOService *provider, SInt32 *score);
+    virtual bool start (IOService *provider);
+    virtual void stop (IOService *provider);
+    virtual void free (void);
+
+    // TODO
+    // virtual IOReturn message(UInt32 type, IOService *provider, void *argument = 0);
+};
