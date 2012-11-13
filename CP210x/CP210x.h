0c9387eeaed9d53debac036cb83e7d9950a5fd38
landon.j.fuller <landon.j.fuller@675f16f6-5868-fe76-6977-53118d06ce56>
2012-11-13 03:08:06 +0000

    Add support for manually configuring DTR and RTS lines.
    This also demonstrates an issue with our use of the IOKit API that will need to be fixed -- calls into IOKit are returning kIOUSBSyncRequestOnWLThread if our own code happens to be called on the work loop thread.

--- CP210x/CP210x.h
+++ CP210x/CP210x.h
@@ -172,6 +172,7 @@ private:
     IOReturn setState (UInt32 state, UInt32 mask, void *refCon, bool haveLock);
     IOReturn watchState (UInt32 *state, UInt32 mask, void *refCon, bool haveLock);
 
+    IOReturn writeCP210xControlLineConfig (UInt32 controlState, UInt32 controlMask);
     IOReturn writeCP210xFlowControlConfig (UInt32 flowState);
     IOReturn writeCP210xDataConfig (uint32_t txParity, bool twoStopBits, uint32_t charLength);
 };
